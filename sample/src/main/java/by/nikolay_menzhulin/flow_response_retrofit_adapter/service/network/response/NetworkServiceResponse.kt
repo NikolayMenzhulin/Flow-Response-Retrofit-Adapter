9ea3cba8c5b0360f594f3ec0cbb3b6eee48cb9bf
Nikolay Menzhulin <NikolayMenzhulin@gmail.com>
2021-03-22 20:14:03 +0300

    Updated sample

---
+++ sample/src/main/java/by/nikolay_menzhulin/flow_response_retrofit_adapter/service/network/response/NetworkServiceResponse.kt
@@ -0,0 +1,5 @@
+package by.nikolay_menzhulin.flow_response_retrofit_adapter.service.network.response
+
+import com.google.gson.annotations.SerializedName
+
+data class NetworkServiceResponse(@SerializedName("result") val result: String)
--- sample/src/main/java/by/nikolay_menzhulin/flow_response_retrofit_adapter/ui/MainActivity.kt
+++ sample/src/main/java/by/nikolay_menzhulin/flow_response_retrofit_adapter/ui/MainActivity.kt
@@ -1,33 +1,106 @@
 package by.nikolay_menzhulin.flow_response_retrofit_adapter.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import by.nikolay_menzhulin.flow_response_retrofit_adapter.R
-import by.nikolay_menzhulin.flow_response_retrofit_adapter.service.GitHubRepository
-import kotlinx.coroutines.flow.collect
+import by.nikolay_menzhulin.flow_response_retrofit_adapter.server.MockServer
+import by.nikolay_menzhulin.flow_response_retrofit_adapter.server.MockServer.ERROR_RESPONSE_JSON
+import by.nikolay_menzhulin.flow_response_retrofit_adapter.server.MockServer.SUCCESS_RESPONSE_JSON
+import by.nikolay_menzhulin.flow_response_retrofit_adapter.service.NetworkServiceRepository
+import by.nikolay_menzhulin.flow_response_retrofit_adapter.ui.base.BaseActivity
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : BaseActivity(R.layout.activity_main) {
 
-    private val gitHubRepository = GitHubRepository()
+    private lateinit var successResponseWithResultBtn: Button
+    private lateinit var successResponseWithoutResultBtn: Button
+    private lateinit var errorResponseBtn: Button
+    private lateinit var cancelRequestBtn: Button
+
+    private val networkServiceRepository = NetworkServiceRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        findViewById<Button>(R.id.get_repo_btn).setOnClickListener { loadDate() }
+        initViews()
+        initListeners()
+        initMockServer()
     }
 
-    private fun loadDate() {
-        lifecycleScope.launch {
-            gitHubRepository.getRepo().collect { response ->
-                Log.d("TEST_TAG", response.toString())
-                val repoName: String = response.getDataOrNull()?.name ?: "EMPTY"
-                Toast.makeText(this@MainActivity, repoName, LENGTH_SHORT).show()
-            }
+    private fun initViews() {
+        successResponseWithResultBtn = findViewById(R.id.success_response_with_result_btn)
+        successResponseWithoutResultBtn = findViewById(R.id.success_response_without_result_btn)
+        errorResponseBtn = findViewById(R.id.error_response_btn)
+        cancelRequestBtn = findViewById(R.id.cancel_request_btn)
+    }
+
+    private fun initListeners() {
+        successResponseWithResultBtn.setOnClickListener { successResponseWithResult() }
+        successResponseWithoutResultBtn.setOnClickListener { successResponseWithoutResult() }
+        errorResponseBtn.setOnClickListener { errorResponse() }
+        cancelRequestBtn.setOnClickListener { cancelRequest() }
+    }
+
+    private fun initMockServer() {
+        lifecycleScope.launch { MockServer.initRetrofit() }
+    }
+
+    private var job: Job? = null
+
+    private fun successResponseWithResult() {
+        MockServer.enqueueResponse(responseCode = 200, responseBody = SUCCESS_RESPONSE_JSON)
+        job = networkServiceRepository.successResponseWithResult()
+            .handleErrors()
+            .onEach { state ->
+                setLoadingState(state.isLoading)
+                when {
+                    state.isLoading -> return@onEach
+                    state.isSuccess -> Toast.makeText(this, state.getData().result, LENGTH_SHORT).show()
+                }
+            }.launchIn()
+    }
+
+    private fun successResponseWithoutResult() {
+        MockServer.enqueueResponse(responseCode = 204)
+        job = networkServiceRepository.successResponseWithoutResult()
+            .handleErrors()
+            .onEach { state ->
+                setLoadingState(state.isLoading)
+                when {
+                    state.isLoading -> return@onEach
+                    state.isSuccess -> Toast.makeText(this, "Success!", LENGTH_SHORT).show()
+                }
+            }.launchIn()
+    }
+
+    private fun errorResponse() {
+        MockServer.enqueueResponse(responseCode = 500, responseBody = ERROR_RESPONSE_JSON)
+        job = networkServiceRepository.errorResponse()
+            .handleErrors()
+            .onEach { state ->
+                setLoadingState(state.isLoading)
+                when {
+                    state.isLoading -> return@onEach
+                    state.isSuccess -> Toast.makeText(this, state.getData().result, LENGTH_SHORT).show()
+                    state.isError -> Toast.makeText(this, "See error in Logcat.", LENGTH_SHORT).show()
+                }
+            }.launchIn()
+    }
+
+    private fun cancelRequest() {
+        job?.apply {
+            cancel()
+            setLoadingState(isLoading = false)
         }
+    }
+
+    private fun setLoadingState(isLoading: Boolean) {
+        successResponseWithResultBtn.isEnabled = !isLoading
+        successResponseWithoutResultBtn.isEnabled = !isLoading
+        errorResponseBtn.isEnabled = !isLoading
     }
 }
